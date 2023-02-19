bbed58b52be8bccdaa2a1e668754b38dec2fee94
Jiayu Ye <106568221+yjy-97@users.noreply.github.com>
2023-02-19 10:04:29 +0800

    Update DRnet.py

--- DRnet.py
+++ DRnet.py
@@ -31,25 +31,6 @@ def setup_seed(seed):
 
 setup_seed(20)
 
-image_transforms = {
-    'train': transforms.Compose([
-        transforms.RandomResizedCrop(size=256, scale=(0.8, 1.0)),
-        transforms.RandomRotation(degrees=15),
-        transforms.RandomHorizontalFlip(),
-        transforms.CenterCrop(size=224),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406],
-                             [0.229, 0.224, 0.225])
-    ]),
-    'valid': transforms.Compose([
-        transforms.Resize(size=256),
-        transforms.CenterCrop(size=224),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406],
-                             [0.229, 0.224, 0.225])
-    ])
-}
-
 
 # Data preprocessing
 # To facilitate the distinction, we unify the naming rules for the facial expression images imitated by the case and control subjects.
